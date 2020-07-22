66a528837353899c3f64c48c6ad60b2fd38822fe
arthuravilez <arthuravilez@gmail.com>
2020-07-22 12:10:15 -0300

    Concluido a atividade de API com flask e Banco de Dados

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from models import Pessoas, Atividades
+from models import Pessoas, Atividades, Usuarios
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
 
+# USUARIOS = {
+#     'rafael':'321',
+#     'galleani':'321'
+# }
+
+# @auth.verify_password
+# def verificacao(login, senha):
+#     if not (login, senha):
+#         return False
+#     return USUARIOS.get(login) == senha
+
+@auth.verify_password
+def verificacao(login, senha):
+    if not (login, senha):
+        return False
+    return Usuarios.query.filter_by(login=login, senha=senha).first()
+
 class Pessoa(Resource):
+    @auth.login_required
     def get(self, nome):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
             response = {
-                'nome': pessoa.nome,
-                'idade': pessoa.idade,
-                'id': pessoa.idade
+                'nome':pessoa.nome,
+                'idade':pessoa.idade,
+                'id':pessoa.id
             }
         except AttributeError:
             response = {
-                'status': 'erro',
-                'mensagem': 'Pessoa nao encontrada'
+                'status':'error',
+                'mensagem':'Pessoa nao encontrada'
             }
-
         return response
 
     def put(self, nome):
@@ -31,9 +50,9 @@ class Pessoa(Resource):
             pessoa.idade = dados['idade']
         pessoa.save()
         response = {
-            'id': pessoa.id,
-            'nome': pessoa.nome,
-            'idade': pessoa.idade
+            'id':pessoa.id,
+            'nome':pessoa.nome,
+            'idade':pessoa.idade
         }
         return response
 
@@ -41,12 +60,13 @@ class Pessoa(Resource):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         mensagem = 'Pessoa {} excluida com sucesso'.format(pessoa.nome)
         pessoa.delete()
-        return {'status': 'sucesso', 'mensagem': mensagem}
+        return {'status':'sucesso', 'mensagem':mensagem}
 
 class ListaPessoas(Resource):
+    @auth.login_required
     def get(self):
         pessoas = Pessoas.query.all()
-        response = [{'id': i.id, 'nome': i.nome, 'idade': i.idade} for i in pessoas]
+        response = [{'id':i.id, 'nome':i.nome, 'idade':i.idade} for i in pessoas]
         return response
 
     def post(self):
@@ -63,9 +83,8 @@ class ListaPessoas(Resource):
 class ListaAtividades(Resource):
     def get(self):
         atividades = Atividades.query.all()
-        response = [{'id': i.id, 'nome': i.nome, 'pessoa': i.pessoa.nome} for i in atividades]
+        response = [{'id':i.id, 'nome':i.nome, 'pessoa':i.pessoa.nome}  for i in atividades]
         return response
-
 
     def post(self):
         dados = request.json
@@ -73,17 +92,15 @@ class ListaAtividades(Resource):
         atividade = Atividades(nome=dados['nome'], pessoa=pessoa)
         atividade.save()
         response = {
-            'pessoa': atividade.pessoa.nome,
-            'nome': atividade.nome,
-            'id': atividade.id
+            'pessoa':atividade.pessoa.nome,
+            'nome':atividade.nome,
+            'id':atividade.id
         }
         return response
-
 
 api.add_resource(Pessoa, '/pessoa/<string:nome>/')
 api.add_resource(ListaPessoas, '/pessoa/')
 api.add_resource(ListaAtividades, '/atividades/')
-
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 # Insere dados na tabela pessoa
 def insere_pessoa():
@@ -24,11 +24,22 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Felipe').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 
 if __name__ == '__main__':
+    insere_usuario('arthur', '1234')
+    insere_usuario('avilez', '4321')
+    consulta_todos_usuarios()
     #insere_pessoas()
     #altera_pessoa()
-    exclui_pessoa()
-    consulta_pessoas()
+    # exclui_pessoa()
+    # consulta_pessoas()
 
 
